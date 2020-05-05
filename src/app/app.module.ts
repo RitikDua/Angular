40fc2efaae12874ed7db47404c6d918363d4900b
Ritik <ritik.cse18@chitkarauniversity.edu.in>
2020-05-05 08:13:16 +0530

    Intialization of App template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { HomeComponent } from './home/home.component';
+import { ExploreComponent } from './explore/explore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    HomeComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule
